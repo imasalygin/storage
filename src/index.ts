6bd751db61e37e402e8f065a4c89563697cfed4e
Ilya Masalygin <ilya.masalygin@gmail.com>
2024-11-23 12:31:01 +0100

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,57 @@
-export type StringKey<T> = Extract<keyof T, string>;
-
-export type Nullable<T> = {
-  [P in keyof T]: T[P] | null;
-}
-
-export type Subscription = () => void;
-
-export type Options = {
-  namespace: string;
+interface CreateStorageOptions<T> {
+  key: string;
   storage?: Storage;
-  stringify?(value: any): string;
-  parse?(value: string): any;
+
+  parse?(value: string): T;
+
+  stringify?(value: T): string;
+
+  defaults?: T | null;
 }
 
-export function create<Type extends Record<string, any>, Key extends StringKey<Type> = StringKey<Type>>(options?: Options) {
+type Subscription = () => void;
+
+export function create<T>(options: CreateStorageOptions<T>){
   const {
-    namespace,
+    key,
     storage = localStorage,
-    stringify = JSON.stringify,
     parse = JSON.parse,
-  } = options ?? {};
+    stringify = JSON.stringify,
+    defaults = null
+  } = options;
 
   const subscriptions = new Set<Subscription>();
 
-  function subscribe(fn: Subscription) {
+  function publish() {
+    subscriptions.forEach((fn) => fn());
+  }
+
+  function subscribe(fn: Subscription){
     subscriptions.add(fn);
     return () => {
       subscriptions.delete(fn);
     }
   }
 
-  function notify() {
-    subscriptions.forEach((fn) => fn());
+  function get (): T | null {
+    const item = storage.getItem(key);
+    return item === null ? defaults : parse(item);
   }
 
-  const prefix = `${namespace}:`;
-
-  function id(key: Key) {
-    return `${prefix}${key}`;
+  function set (value: T) {
+    storage.setItem(key, stringify(value));
+    publish();
   }
 
-  function set<K extends Key>(key: K, value: Type[K]): void {
-    storage.setItem(id(key), stringify(value));
-    notify();
-  }
-
-  function has(key: Key): boolean {
-    return id(key) in storage
-  }
-
-  function get<K extends Key>(key: K): Type[K] | null;
-  function get<K extends Key>(key: K[]): Nullable<Pick<Type, K>>;
-  function get<K extends Key>(key: K | K[]): unknown {
-    if (Array.isArray(key)) {
-      return key.reduce((acc, key) => {
-        acc[key] = get(key);
-        return acc;
-      }, {} as Nullable<Pick<Type, K>>);
-    } else {
-      const value = storage.getItem(id(key));
-      return value === null ? null : parse(value);
+  globalThis.addEventListener('storage', (event) => {
+    if (event.key === key && event.storageArea === storage) {
+      publish();
     }
+  });
+
+  return {
+    get,
+    set,
+    subscribe
   }
-
-  function remove(key: Key): void {
-    if (has(key)) {
-      storage.removeItem(id(key));
-      notify();
-    }
-  }
-
-  function keys(): Key[] {
-    const keys: Key[] = [];
-
-    for (let i = 0; i < storage.length; i++) {
-      const key = storage.key(i);
-      if (key && key.startsWith(prefix)) {
-        keys.push(key.substring(prefix.length) as Key);
-      }
-    }
-
-    return keys;
-  }
-
-  function clear(): void {
-    keys().forEach(remove);
-  }
-
-  if (typeof window !== 'undefined' && [localStorage, sessionStorage].includes(storage)) {
-    window.addEventListener('storage', (event) => {
-      if (event.key?.startsWith(prefix) && event.storageArea === storage) {
-        notify();
-      }
-    });
-  }
-
-  const methods = { set, get, has, remove, clear, subscribe, keys };
-
-  return new Proxy(methods, {
-    get(target, prop: Key & typeof target) {
-      return prop in target ? target[prop] : target.get(prop);
-    }
-  }) as typeof methods & Nullable<Type>;
-}
+};
